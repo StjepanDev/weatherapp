575e8fad31a39d4a441ac998ece66ec747770f2f
StjepanDev <grunf69@yahoo.com>
2021-07-04 21:03:06 +0200

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import nav from '../css/Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-light navbar-expand-md">
+        <Container>
+          <a className="navbar-brand" href="#">
+            Dhmz
+          </a>
+          <button
+            data-toggle="collapse"
+            className="navbar-toggler"
+            data-target="#navcol-1"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navcol-1">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  First Item
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Second Item
+                </a>
+              </li>
+              <li className="nav-item" />
+            </ul>
+          </div>
+        </Container>
+      </nav>
+    </div>
+  );
+}
