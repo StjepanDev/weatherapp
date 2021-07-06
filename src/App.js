c179cbc9a48b3221161bc3db415906875fa8f43c
StjepanDev <grunf69@yahoo.com>
2021-07-06 21:12:05 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React from 'react';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Container from './components/Container';
 
+const API_KEY = 'f7kj9y7k2uSDtspu9ME7jnwXJUGY9Xqe';
+
 function App() {
+  const getGradovi = async () => {
+    const res = await fetch('http://localhost:3000/gradovi');
+    const gradovi = await res.json();
+    console.log(gradovi);
+  };
+  getGradovi();
+
+  // const getData = async () => {
+  //   const response = await fetch(
+  //     `https://api.tomorrow.io/v4/timelines?location=45.98529171943665,15.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=${API_KEY}`
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  // };
+  // getData();
   return (
     <div>
       <Navbar />
